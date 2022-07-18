901a5836e3c773ab1c2f96f1fc3afca8e4a803f0
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:55:27 +0700

    tugas 20 js
    untuk niomic

---
+++ Tugas20/Tugas20.js
@@ -0,0 +1,9 @@
+function mat() {
+  var isi = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(isi.match("are"));
+
+  var isi = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(isi.match("words"));
+}
+
+mat();
